77a79520d5df6be78a6b962b71af1acc9a25a0cf
BarbaraMoran <barbaramoranrr@gmail.com>
2022-01-23 22:16:56 +0100

    add http request protocol and observable

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from "@angular/core";
+import { ThisReceiver } from "@angular/compiler";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
 import { IProduct } from "./product";
 import { ProductService } from "./product.service";
 
@@ -8,13 +10,16 @@ import { ProductService } from "./product.service";
   styleUrls: ['./product-list.component.css']
 })
 
-export class ProductListComponent implements OnInit{
+export class ProductListComponent implements OnInit, OnDestroy{
   pageTitle: string = 'Product List';
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
+  errorMessage: string = "";
+  sub!: Subscription | undefined;
 
   private _listFilter: string = "";
+
 
   get listFilter():string {
 
@@ -56,9 +61,18 @@ export class ProductListComponent implements OnInit{
   }
 
   ngOnInit(): void {
-    this.products =  this.productService.getProducts();
-    this.filteredProducts = this.products;
+    this.sub =  this.productService.getProducts().subscribe({
+      next: products => {
+        this.products = products;
+        this.filteredProducts = this.products;
+      },
 
+      error: err => this.errorMessage = err
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.sub?.unsubscribe();
   }
 
 
